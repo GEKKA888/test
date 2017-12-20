3a54263bd026abab9ab8ad58f2c52f22cbdb3702
正田孝平 <shoda@shoudakouheinoMacBook-Air.local>
2017-12-20 17:25:13 +0900

    add status1

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,15 +2,18 @@ FactoryGirl.define do
   factory :task do
     title "hoge"
     content "hogehoge"
+    deadline "2018-01-01"
   end
   factory :taska, class: Task do
     title "fugafugafugafugafugafugafugafugafugafugafugafugafuga"
     content "hogehoge"
+    deadline "2018-01-01"
   end
   factory :taskb, class: Task do
     title "hoge"
     content "fugafugafugafugafugafugafugafugafugafugafugafugafuga
     fugafugafugafugafugafugafugafugafugafugafugafugafuga
     fugafugafugafugafugafugafugafugafugafugafugafugafuga"
+    deadline "2018-01-01"
   end
 end
